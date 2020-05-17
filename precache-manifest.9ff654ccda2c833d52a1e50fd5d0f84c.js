54258bc6b2483b58d9059e7e4d06a1e40fa7191c
GermanTorales <germantorales73@gmail.com>
2020-05-17 13:35:34 -0300

    Updates

---
+++ precache-manifest.9ff654ccda2c833d52a1e50fd5d0f84c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14060688dc8abcb8daf999ac3cec77e8",
+    "url": "/ego-ui/index.html"
+  },
+  {
+    "revision": "6a840dc1d6eae154611e",
+    "url": "/ego-ui/static/css/main.cbc61829.chunk.css"
+  },
+  {
+    "revision": "2d67305cc86910d98491",
+    "url": "/ego-ui/static/js/2.9c960e9b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ego-ui/static/js/2.9c960e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a840dc1d6eae154611e",
+    "url": "/ego-ui/static/js/main.e926e730.chunk.js"
+  },
+  {
+    "revision": "e817deade4ea4c8b6282",
+    "url": "/ego-ui/static/js/runtime-main.8e370e2e.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/ego-ui/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/ego-ui/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/ego-ui/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  }
+]);
